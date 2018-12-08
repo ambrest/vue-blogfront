473272aa9ef483153db3eb2d894f9bc01fd810c3
Simonwep <toports@gmx.de>
2018-12-08 20:38:46 +0100

    Update project structure

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 // Routes
-import Home     from './vue/pages/home/Home';
-import Login    from './vue/pages/login/Login';
-import Register from './vue/pages/register/Register';
-import Admin    from './vue/pages/admin/Admin';
-import New      from './vue/pages/new/New';
-import Post     from './vue/pages/post/Post';
+import Home       from './vue/pages/home/Home';
+import Login      from './vue/pages/login/Login';
+import Register   from './vue/pages/register/Register';
+import Admin      from './vue/pages/admin/Admin';
+import CreatePost from './vue/pages/createpost/CreatePost';
+import ViewPost   from './vue/pages/viewpost/ViewPost';
 
 export default [
     {path: '/', component: Home},
     {path: '/login', component: Login},
     {path: '/register', component: Register},
     {path: '/admin', component: Admin},
-    {path: '/new', component: New},
-    {path: '/post/*', component: Post}
+    {path: '/new', component: CreatePost},
+    {path: '/post/*', component: ViewPost}
 ];
